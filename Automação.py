b2debf1d3a85aeef4186d190bc94dad826fa6d0b
KaiqueBatista <kaique_batista@outlook.com>
2025-03-20 15:29:35 -0300

    Update Automação.py

--- Automação.py
+++ Automação.py
@@ -1,55 +1,41 @@
 #PASSO 1 - abrir o site
 
-import pyautogui #"import pyautogui/pandas/time" - sempre usar o 'import' para usar a biblioteca
+import pyautogui 
+import time 
 
-import time # 'time' serve para pedir para o sistema esperar um certo tempo
-
-pyautogui.PAUSE = 1 # tempo de espera entre um comando e outro
-
-pyautogui.press("win") # pressionar a tecla windowns no teclado
-
-pyautogui.write("chrome") # escrever o navegador na pesquisa 
-
-pyautogui.press("enter") # pressionar a tecla enter
-
-pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login") # escrever o site que se quer entrar
-
+pyautogui.PAUSE = 1
+pyautogui.press("win") 
+pyautogui.write("chrome")
+pyautogui.press("enter") 
+pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
 pyautogui.press("enter")
-
-time.sleep(2) # aguardar 2 seg. para o site carregar 
+time.sleep(2)
 
 #PASSO 2 - fazer login
 # para descobrir a posição do mouse, usar o: 'print(pyautogui.position())
 
-pyautogui.click(x=701, y=379) # posição do campo de e-mail no site
-
-pyautogui.write("qualquer-email@qualquer-coisa.com") # escrever o e-mail para login
-
-pyautogui.press("tab") # pressionar tab para seguir ao proximo campo de preenchimento (senha)       
-
-pyautogui.write("123") # digitar senha
-
+pyautogui.click(x=701, y=379)
+pyautogui.write("qualquer-email@qualquer-coisa.com") 
+pyautogui.press("tab")      
+pyautogui.write("123")
 pyautogui.press("tab")
-
 pyautogui.press("enter")
-
 time.sleep(2)
  
 # PASSO 3 - importar base de dados
 # criar uma variavel para armazenar informacoes do banco de dados = tabela
 
 import pandas
+tabela = pandas.read_csv("produtos.csv")
 
-tabela = pandas.read_csv("produtos.csv") # cvs é o tipo de arquivo, pode mudar conforme o arquivo
+# PASSO 4 - Cadastrar 1 Produto antes de todos, após isso, usar o 'for' para repetir
 
-# PASSO 4 - Cadastrar 1 Produto antes de todos - MOLO000251,Logitech,Mouse,1,25.95,6.50.. apos isso, usar o 'for' para repetir
+for linha in tabela.index:
 
-for linha in tabela.index: # tudo abaixo desse cod. vai repetir até finalizar (PASSO 5)
-
-    pyautogui.click(x=668, y=248) # clicar no campo de preenchimento
+    pyautogui.click(x=668, y=248) 
 
     # codigo
-    codigo = tabela.loc[linha, "codigo"] #usar 'tabela.loc' para localizar algo na tabela + [linha, coluna] para selecionar de onde deve tirar as informações
+    codigo = tabela.loc[linha, "codigo"]
     pyautogui.write(codigo)
     pyautogui.press('tab')
 
